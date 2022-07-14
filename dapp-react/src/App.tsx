27925e1975b91d4312c571697c943838bbe82104
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-07-14 14:18:32 -0300

    initial setup

---
+++ dapp-react/src/App.tsx
@@ -0,0 +1,12 @@
+import Example from './component';
+
+function App() {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold underline'>Hello world</h1>
+            <Example />
+        </div>
+    );
+}
+
+export default App;
