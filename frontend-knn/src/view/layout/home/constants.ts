338d3ec55005dd9bf93c9ff61a4b1bd4ab7c16e6
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-08-04 19:38:19 -0300

    app view layer implemented

---
+++ frontend-knn/src/view/layout/home/constants.ts
@@ -0,0 +1,40 @@
+
+export const id = {
+    sendInputForm: {
+        main: "sendInputForm",
+        ageInput: "Age",
+        sexInput: "Sex",
+        embarkedInput: "Embarked",
+    },
+};
+
+export const string = {
+    resultPreview: {
+        title: "Result preview",
+        idleFeedback: "Use the form to get started! 🧊🚢",
+        pendingFeedback: "Waiting for result...",
+        survivedFeeback: "YOU SURVIVED!",
+        sankFeedback: "YOU SANK!",
+    },
+    sendInputForm: {
+        ageInputText: "Age",
+        sexInputText: "Gender",
+        embarkedInputText: "Embarked from",
+        submitButtonText: "See result",
+        clearButtonText: "Clear result",
+        loadingButtonText: "Waiting...",
+        description:
+            "Try if you would survive or sink in the Titanic crash! 👀",
+    },
+    sendInputFeedback: {
+        requestStarted:
+            "Sending infos, wait please. It may take a few minutes.",
+        web3OnboardError:
+            "You need to connect the wallet to proceed with this action!",
+        onError: "Sorry! An error occurred while send infos, try again later.",
+    },
+    fetchNoticesFeedback: {
+        onSucess: "Your result is ready!",
+        onError: "Sorry! An error occurred while get result, try again later.",
+    },
+};
