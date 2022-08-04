4de94c0c43279d8c20414597e05f75d5c94c10c5
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-08-04 19:38:19 -0300

    app view layer implemented

---
+++ frontend-knn/src/view/atomic/layout.org/layout.mol.ts
@@ -0,0 +1,38 @@
+import styled, { css } from "styled-components";
+import { border, color, radius, spacing } from "../styleguide.atm";
+
+interface Layout {
+    isFluid?: boolean;
+}
+
+const sharedDefaultCss = css`
+    padding: ${spacing.padding.layout};
+`;
+
+export const Header = styled.header`
+    ${sharedDefaultCss}
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const Main = styled.main`
+    ${sharedDefaultCss}
+`;
+
+export const BoxWrapper = styled.div<Layout>`
+    padding: 0 ${spacing.padding.md};
+    border: ${border.small} ${color.mediumMain};
+    border-radius: ${radius.lg};
+    background: linear-gradient(
+        to top left,
+        ${color.main} 60%,
+        ${color.mediumMain} 100%
+    );
+    ${({ isFluid }) => `
+        ${isFluid ? "width: 100%; height: 100%;" : ""}
+    `}
+`;
+
