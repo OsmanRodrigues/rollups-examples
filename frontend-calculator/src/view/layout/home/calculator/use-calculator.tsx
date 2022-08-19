46f6e76d55bb37794f399169ee9ff72f5cd62796
Marcus Souza <souza.mvsl@gmail.com>
2022-08-19 11:25:10 -0300

    calculator integrated with wallet and layout ongoing

--- frontend-calculator/src/view/layout/home/calculator/use-calculator.tsx
+++ frontend-calculator/src/view/layout/home/calculator/use-calculator.tsx
@@ -2,10 +2,33 @@ import React, { useState } from "react";
 
 //Custom Hook
 /**
- * Use cases : Numeral Operador Numeral
- *             Numeral Operador
- *             Numeral Operador Numeral Operador
+ * Use cases : Numeral Operator Numeral
+ *             Numeral Operator
+ *             Numeral Operator Numeral Operator
+ * Special operators:
+"1/x = 1 / numeral
+"x²" = numeral ^ 2
+"√"  = sqrt(numeral)
+"%"  = percent * total %
  */
+
+enum CommonOperations{
+    "+" = "+",
+    "-" = "-",
+    "/" = "/",
+    "*" = "*",
+    "^" = "^"
+}
+enum Delimiter{
+    "." = "."
+}
+
+enum SpecialOperations{
+"1/x" = "1/x",
+"x²" = "x²",
+"√"  = "√",
+"%"  = "%"
+}
 
 export const useCalculator = () =>{
 
@@ -15,31 +38,103 @@ export const useCalculator = () =>{
     //TODO - Polish array treatment when operation is empty
     currentOperation.reduce((total,current)=> total + current)
 
+
     const handleCommonOperation = (value: string,currentOperation: typeof operation) =>{
         if(!currentOperation.length){
             setOperation([value])
         }else{
-            const lastElement = currentOperation[currentOperation.length - 1]
-            switch (lastElement) {
-                case "+"||"-"||"/"||"*":
-                    if(Number(value)){
+            const lastIndex = currentOperation.length -1;
+            const lastElement = currentOperation[lastIndex]
+
+            const incrementLastElement =(currentValue: string)=>{
+                setOperation((prevState)=>{
+                    const concat = lastElement + currentValue
+                    const prevStateCopy = [...prevState]
+                    prevStateCopy[lastIndex] = concat;
+                    return prevStateCopy
+                })
+
+            }
+
+
+            if(Number.isNaN(Number(value))){
+                const lastElementIsNaN = Number.isNaN(Number(lastElement))
+
+                if(value === Delimiter["."]){
+                    if(!lastElementIsNaN){
+                        incrementLastElement(value)
+                    }
+               }else if(Object.keys(CommonOperations).includes(value)){
+                    if(!lastElementIsNaN)
                         setOperation((prevState)=>{
                             const prevStateCopy = [...prevState, value]
                             return prevStateCopy
+                            })
+               }
+            }else{
+                if(Object.keys(CommonOperations).includes(lastElement)){
+                    setOperation((prevState)=>{
+                        const prevStateCopy = [...prevState, value]
+                        return prevStateCopy
                         })
-                    }//TO-DO
-                    break;
-
-                default:
-                    break;
+                }else if(!Number.isNaN(Number(lastElement))){
+                    incrementLastElement(value)
+                }
             }
         }
 
 
     }
+    const handleClear = (clearType : "CE"|"C"|"<", currentOperation: typeof operation)=>{
+        if(!currentOperation.length){
+            return null
+        }
+
+        const lastIndex = currentOperation.length -1;
+        const lastElement = currentOperation[lastIndex]
+
+
+
+        switch (clearType) {
+            case "CE":
+                setOperation(()=>{
+                    const currentOperationCopy = [...currentOperation]
+                    currentOperation.pop()
+
+                    return currentOperationCopy
+                    })
+                break;
+            case "C":
+                setOperation(()=>{
+                    return []
+                    })
+                break;
+            case "<":
+                if(Number.isNaN(Number(lastElement))){
+                    return null
+                }
+                setOperation(()=>{
+                    const currentOperationCopy = [...currentOperation]
+                    if(lastElement.length > 1){
+                        const newLastElement = lastElement.slice(0,lastElement.length -1)
+                        currentOperation.pop()
+                        currentOperation.push(newLastElement)
+
+                    }else{
+                        currentOperation.pop()
+                    }
+                    return currentOperationCopy
+                    })
+                break;
+            default:
+                break;
+        }
+    }
     return {
         operation,
         getOperation,
-        handleCommonOperation
+        handleCommonOperation,
+        handleClear
     }
+
 }
