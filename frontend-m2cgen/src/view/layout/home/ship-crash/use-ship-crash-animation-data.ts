bd3c466f44c672be0c94463c80596ee23a883635
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-09-08 19:35:55 -0300

    use ship crash animation responsive ship motion implemented

--- frontend-m2cgen/src/view/layout/home/ship-crash/use-ship-crash-animation-data.ts
+++ frontend-m2cgen/src/view/layout/home/ship-crash/use-ship-crash-animation-data.ts
@@ -1,14 +1,23 @@
 import { HTMLMotionProps } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { UseServiceState } from "../../../../controller/use-service/use-service.hook";
+import { genTimerPromise } from "../../../../utils/timer-promise";
 
-type UseShipCrashAnimationDataReturn = [
-    HTMLMotionProps<'div'>, HTMLMotionProps<'div'>
-];
+interface UseShipCrashAnimationDataReturn {
+    shipMotion: HTMLMotionProps<"div">;
+    icebergMotion: HTMLMotionProps<"div">;
+    status: "idle"|"pending" |"ready";
+}
 
+const SHIP_INITIAL_X = 200;
+const PIXEL_DIVISOR = 8;
+const BREAKPOINT = {
+    DESKTOP: 1368,
+    MOBILE: 768
+};
 const initialShipMotionProps: HTMLMotionProps<"div"> = {
     initial: {
-        x: -200,
+        x: -SHIP_INITIAL_X,
         y: -14,
     },
     animate: {
@@ -25,15 +34,54 @@ const initialIcebergMotionProps: HTMLMotionProps<"div"> = {
         y: 22,
     },
 };
+const getShipResponsiveX = (breakpoint: number, size: number) =>
+    ((size*SHIP_INITIAL_X) / breakpoint) -
+    ((breakpoint-size) / (size > breakpoint ? PIXEL_DIVISOR*(1+(breakpoint-size)) : PIXEL_DIVISOR));
 
 export const useShipCrashAnimationData = (
-    status: UseServiceState<any>["status"]
+    requestStatus: UseServiceState<any>["status"]
 ): UseShipCrashAnimationDataReturn => {
-    const [shipMotion, setShipMotion] = useState(()=> initialShipMotionProps);
-    useEffect(() => {
-        const { innerHeight, innerWidth } = window;
-        console.log({ innerHeight, innerWidth });
+    const [status, setStatus] =
+        useState<UseShipCrashAnimationDataReturn['status']>('idle');
+    const [shipMotion, setShipMotion] = useState(() => initialShipMotionProps);
+
+    useLayoutEffect(() => {
+        const updateShipMotion = async () => {
+            setStatus("pending");
+            await genTimerPromise(1000);
+            setShipMotion((prev) => {
+                const { innerWidth } = window;
+                const breakpointFallback =
+                    innerWidth < BREAKPOINT.MOBILE
+                        ? BREAKPOINT.MOBILE
+                        : BREAKPOINT.DESKTOP;
+
+                const newShipMotion =
+                    typeof prev.initial === "object"
+                        ? {
+                              ...prev,
+                              initial: {
+                                  ...prev.initial,
+                                  x: -getShipResponsiveX(
+                                      breakpointFallback,
+                                      innerWidth
+                                  ),
+                              },
+                          }
+                        : {};
+                return newShipMotion;
+            });
+            setStatus("ready");
+        }
+        window.addEventListener("resize", updateShipMotion);
+        updateShipMotion();
+
+        return () => window.removeEventListener("resize", updateShipMotion);
     }, []);
 
-    return [shipMotion, initialIcebergMotionProps];
+    return {
+        shipMotion,
+        icebergMotion: initialIcebergMotionProps,
+        status
+    };
 };
