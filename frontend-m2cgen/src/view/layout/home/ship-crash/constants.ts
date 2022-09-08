244ecc9599340df063eef93339c4637f7e2f6079
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-09-08 20:23:09 -0300

    ship crash animantion request resolved status adjusted

---
+++ frontend-m2cgen/src/view/layout/home/ship-crash/constants.ts
@@ -0,0 +1,7 @@
+export const SHIP_INITIAL_X = 200;
+export const PIXEL_DIVISOR = 8;
+export const BREAKPOINT = {
+    DESKTOP: 1368,
+    MOBILE: 768,
+};
+export const RESOLVED_REQUEST_TRANSITION_DURATION = 3;
--- frontend-m2cgen/src/view/layout/home/ship-crash/use-ship-crash-animation-data.ts
+++ frontend-m2cgen/src/view/layout/home/ship-crash/use-ship-crash-animation-data.ts
@@ -1,27 +1,26 @@
 import { HTMLMotionProps } from "framer-motion";
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { UseServiceState } from "../../../../controller/use-service/use-service.hook";
 import { genTimerPromise } from "../../../../utils/timer-promise";
+import {
+    SHIP_INITIAL_X,
+    PIXEL_DIVISOR,
+    BREAKPOINT,
+    RESOLVED_REQUEST_TRANSITION_DURATION
+} from "./constants";
 
 interface UseShipCrashAnimationDataReturn {
     shipMotion: HTMLMotionProps<"div">;
     icebergMotion: HTMLMotionProps<"div">;
     status: "idle"|"pending" |"ready";
 }
-
-const SHIP_INITIAL_X = 200;
-const PIXEL_DIVISOR = 8;
-const BREAKPOINT = {
-    DESKTOP: 1368,
-    MOBILE: 768
-};
 const initialShipMotionProps: HTMLMotionProps<"div"> = {
     initial: {
         x: -SHIP_INITIAL_X,
         y: -14,
     },
     animate: {
-        x: 50,
+        x: 30
     },
     transition: {
         duration: 60,
@@ -39,45 +38,70 @@ const getShipResponsiveX = (breakpoint: number, size: number) =>
     ((breakpoint-size) / (size > breakpoint ? PIXEL_DIVISOR*(1+(breakpoint-size)) : PIXEL_DIVISOR));
 
 export const useShipCrashAnimationData = (
-    requestStatus: UseServiceState<any>["status"]
+    requestStatus: UseServiceState<any>['status']
 ): UseShipCrashAnimationDataReturn => {
     const [status, setStatus] =
         useState<UseShipCrashAnimationDataReturn['status']>('idle');
     const [shipMotion, setShipMotion] = useState(() => initialShipMotionProps);
 
     useLayoutEffect(() => {
-        const updateShipMotion = async () => {
-            setStatus("pending");
-            await genTimerPromise(1000);
-            setShipMotion((prev) => {
-                const { innerWidth } = window;
-                const breakpointFallback =
-                    innerWidth < BREAKPOINT.MOBILE
-                        ? BREAKPOINT.MOBILE
-                        : BREAKPOINT.DESKTOP;
+        if (requestStatus === 'pending') {
+            const updateShipMotion = async () => {
+                setStatus('pending');
+                await genTimerPromise(1000);
+                setShipMotion((prev) => {
+                    const { innerWidth } = window;
+                    const breakpointFallback =
+                        innerWidth < BREAKPOINT.MOBILE
+                            ? BREAKPOINT.MOBILE
+                            : BREAKPOINT.DESKTOP;
 
+                    const newShipMotion =
+                        typeof prev.initial === 'object'
+                            ? {
+                                ...prev,
+                                initial: {
+                                    ...prev.initial,
+                                    x: -getShipResponsiveX(
+                                        breakpointFallback,
+                                        innerWidth
+                                    ),
+                                },
+                            }
+                            : {};
+                    return newShipMotion;
+                });
+                setStatus('ready');
+            }
+            window.addEventListener('resize', updateShipMotion);
+            updateShipMotion();
+
+            return () => window.removeEventListener('resize', updateShipMotion);
+        }
+    }, [requestStatus]);
+
+    useEffect(() => {
+        if (requestStatus === 'resolved') {
+            setStatus('pending');
+            setShipMotion(prev => {
                 const newShipMotion =
                     typeof prev.initial === "object"
                         ? {
                               ...prev,
-                              initial: {
-                                  ...prev.initial,
-                                  x: -getShipResponsiveX(
-                                      breakpointFallback,
-                                      innerWidth
-                                  ),
+                              animate: {
+                                  x: 50,
+                              },
+                              transition: {
+                                  duration:
+                                      RESOLVED_REQUEST_TRANSITION_DURATION,
                               },
                           }
                         : {};
                 return newShipMotion;
             });
-            setStatus("ready");
+            setStatus('ready');
         }
-        window.addEventListener("resize", updateShipMotion);
-        updateShipMotion();
-
-        return () => window.removeEventListener("resize", updateShipMotion);
-    }, []);
+    }, [requestStatus])
 
     return {
         shipMotion,
