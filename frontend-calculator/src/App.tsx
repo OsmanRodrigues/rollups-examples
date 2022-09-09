ec040208603a13f684035b5c7f441fbc8b5d66a3
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-09-09 18:49:23 -0300

    calculator history context and component implemented

--- frontend-calculator/src/App.tsx
+++ frontend-calculator/src/App.tsx
@@ -3,6 +3,7 @@ import { HomeView } from "./view/layout/home/home-view";
 import { setConfiguration } from "react-grid-system";
 import { ToastContainer } from "react-toast";
 import { initWeb3Wallet } from "./config/web3-wallet.setup";
+import { CalculatorHistoryProvider } from "./view/layout/home/calculator/calculator-history/calculator-history.context";
 
 
 
@@ -15,7 +16,9 @@ export const App = () => {
     return (
         <>
             <GlobalStyle />
-            <HomeView />
+            <CalculatorHistoryProvider>
+                <HomeView />
+            </CalculatorHistoryProvider>
             <ToastContainer position="bottom-center" delay={6000} />
         </>
     );
