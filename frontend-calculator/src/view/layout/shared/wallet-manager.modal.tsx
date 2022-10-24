dc7c7a6083613d5b5f6407046a96fcca0416f973
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-10-24 11:20:27 -0300

    modal layout and wallet manager modal components adjusted

--- frontend-calculator/src/view/layout/shared/wallet-manager.modal.tsx
+++ frontend-calculator/src/view/layout/shared/wallet-manager.modal.tsx
@@ -1,3 +1,14 @@
+// Copyright 2022 Cartesi Pte. Ltd.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 import { useConnectWallet, useSetChain } from "@web3-onboard/react";
 import { FC } from "react";
 import { Col, Row } from "react-grid-system";
@@ -6,26 +17,26 @@ import { FieldsetWrapper } from "../../atomic/form.org/form.mol";
 import { Input } from "../../atomic/form.org/input.mol";
 import { Modal, IModal } from "../../atomic/layout.org/modal.mol/modal.mol";
 import { Separator } from "../../atomic/layout.org/separator.mol/separator.atm";
-import { H1 } from "../../atomic/typography.mol";
 import { string } from "./constants";
 
 const modalString = string.manageWalletModal;
 
-export const WalletManagerModal: FC<IModal> = ({
-    isOpen, onClose
+type IWalletManagerModal = Pick<IModal, "isOpen" | "onClose">;
+
+export const WalletManagerModal: FC<IWalletManagerModal> = ({
+    isOpen,
+    onClose,
 }) => {
-    const [{wallet, connecting}, connect, disconnect] = useConnectWallet();
-    const [{chains, connectedChain, settingChain}, setChain] = useSetChain();
+    const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
+    const [{ chains, connectedChain, settingChain }, setChain] = useSetChain();
 
     return (
-        <Modal isOpen={isOpen} onClose={onClose}>
-            <Row justify="center">
-                <Col xs="content">
-                    <H1 color="dark">
-                        {modalString.title}
-                    </H1>
-                </Col>
-            </Row>
+        <Modal
+            isOpen={isOpen}
+            title={modalString.title}
+            labelledBy={modalString.aria.labelledById}
+            onClose={onClose}
+        >
             <Separator large />
             {!!wallet ? (
                 <>
@@ -89,4 +100,4 @@ export const WalletManagerModal: FC<IModal> = ({
             <Separator />
         </Modal>
     );
-}
+};
