227c1ef7a9a954aeba3bf325345cf1e4d54f25bf
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-10-24 11:33:30 -0300

    about modal implemented

--- frontend-calculator/src/view/layout/shared/constants.ts
+++ frontend-calculator/src/view/layout/shared/constants.ts
@@ -1,4 +1,24 @@
 export const string = {
+    aboutButton: {
+        text: "About",
+    },
+    aboutModal: {
+        title: "About",
+        aria: {
+            labelledById: "about-modal",
+        },
+        textTitle: "Web3 Calculator - An DApp by Locus Powered By Cartesi",
+        textContent1: `This DApp is one from a series of DApps born from the partnership of Locus Custom Software and Cartesi.
+            The main objective was to bring common concepts from the web2 to the web3 through the Cartesi rollups technology.`,
+        textContent2: `
+        This Calculator DApp uses Python libraries to compute a plethora of expressions.
+        All the computation is done inside the blockchain, thanks to the Cartesi API.
+        Bringing those kind of concepts to the blockchain, one of the beneficts of using the Cartesi solution.
+        `,
+        CTAButton: {
+            text: "OK",
+        },
+    },
     header: "Locus DApp | Powered By Cartesi",
     manageWalletButton: {
         text: "Manage wallet",
