734f13531f25ae775d96509455a0864a7f429502
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-10-24 10:35:44 -0300

    onboard tour module implemented

---
+++ frontend-calculator/src/view/layout/home/onboard-tour/helpers.ts
@@ -0,0 +1,51 @@
+import { ShepherdButtonWithType } from "react-shepherd";
+import { onboardTourCSSClass } from "./onboard-tour.style";
+
+enum StepType {
+    FirstStep = "FirstStep",
+    LastStep = "LastStep",
+    CommonStep = "CommonStep",
+}
+
+const buttons: ShepherdButtonWithType[] = [
+    {
+        classes: onboardTourCSSClass["onboard-tour-button-secondary"],
+        text: "Exit",
+        type: "cancel",
+    },
+    {
+        classes: onboardTourCSSClass["onboard-tour-button-primary"],
+        text: "Back",
+        type: "back",
+    },
+    {
+        classes: onboardTourCSSClass["onboard-tour-button-primary"],
+        text: "Next",
+        type: "next",
+    },
+];
+
+export const resetTourScroll = () =>
+    new Promise(function (resolve) {
+        setTimeout(function () {
+            window.scrollTo(0, 0);
+            resolve(void 0);
+        }, 500);
+    });
+
+export const getStepButtons = (
+    stepType?: keyof typeof StepType
+): ShepherdButtonWithType[] =>
+    buttons.map((button) => {
+        if (stepType === "FirstStep" && button.type === "back")
+            return {
+                ...button,
+                disabled: true,
+            };
+        else if (stepType === "LastStep" && button.type === "next")
+            return {
+                ...button,
+                disabled: true,
+            };
+        return button;
+    });
