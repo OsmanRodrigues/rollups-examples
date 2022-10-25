bc117e461e1fa8933b549d3833e333060a3b6173
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-10-25 16:43:47 -0300

    frontend knn copyright labels added

--- frontend-knn/src/view/layout/home/chart-draw/use-parse-chart-draw-data.ts
+++ frontend-knn/src/view/layout/home/chart-draw/use-parse-chart-draw-data.ts
@@ -1,3 +1,14 @@
+// Copyright 2022 Cartesi Pte. Ltd.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 import { useState, useEffect } from "react";
 import { UseFormSetError, UseFormClearErrors } from "react-hook-form";
 import { SendInputData } from "../../../../controller/send.controller";
@@ -9,10 +20,10 @@ export const useParseChartDrawData = (
     clearError: UseFormClearErrors<SendInputData>
 ): SendInputData => {
     const [parsedFormData, setParsedFormData] = useState<typeof formData>({
-        pl: '',
-        pw: '',
-        sl: '',
-        sw: '',
+        pl: "",
+        pw: "",
+        sl: "",
+        sw: "",
     });
 
     useEffect(() => {
@@ -32,10 +43,12 @@ export const useParseChartDrawData = (
 
     const chartDrawData = {
         ...parsedFormData,
-        pl: +parsedFormData.pl > +parsedFormData.sl
+        pl:
+            +parsedFormData.pl > +parsedFormData.sl
                 ? parsedFormData.sl
                 : parsedFormData.pl,
-        pw: +parsedFormData.pw > +parsedFormData.sw
+        pw:
+            +parsedFormData.pw > +parsedFormData.sw
                 ? parsedFormData.sw
                 : parsedFormData.pw,
     };
